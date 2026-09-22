d1f11ffa35a72bc02f7504b8e9ba77a779c0eb56
Alan Sempruch <32146438+aSempruch@users.noreply.github.com>
2026-09-22 18:10:31 -0400

    Use neutral gray surfaces with Melange

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,11 @@ return {
     config = function()
       vim.o.termguicolors = true
       vim.o.background = 'dark'
+      -- Keep Melange's syntax colors, but use Darcula-like neutral surfaces.
+      local grays = require('melange/palettes/dark').a
+      grays.bg = '#2B2B2B'
+      grays.float = '#333333'
+      grays.sel = '#414141'
       vim.cmd.colorscheme 'melange'
     end,
   },
